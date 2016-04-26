28a1de3ab474792ee41cb02853ca560333762711
bvo773 <binhvo07@gmail.com>
2016-04-26 14:58:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -181,10 +181,10 @@ legend.onAdd = function (map) {
 //Customize the legend for each tab
 legend.update = function (props) {
   var length = 5,
-    grades2 = [.03, .05, 1, 5, 10],
-    grades3 = [.02, .05, 1, 5, 10],
+    grades2 = [.3, .5, 1, 5, 10],
+    grades3 = [.2, .5, 1, 5, 10],
     grades4 = [.2, .5, 5, 12, 15],
-    grades5 = [.05, 1, 5, 15, 20],
+    grades5 = [.5, 1, 5, 15, 20],
     labels = [],
     from, to;
   for (var i = 0; i < length; i++) {
