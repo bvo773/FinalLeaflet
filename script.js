a31315dce4128db0a0d54230f4cee0fc96552f9f
bvo773 <binhvo07@gmail.com>
2016-04-27 11:38:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -106,6 +106,14 @@ function getColor(d) {
     } else {
       return "#ffffff";
     }
+  } else if(tabnum.localeCompare("1") == 0){
+    if (d > -1) {
+      return d > 36  ? '#a63603' :
+      d > 5  ? '#e6550d' :
+      d > 2   ? '#fd8d3c' :
+      d > 1   ? '#fdbe85' :
+      '#feedde';
+    }
   } else {
     return '#fdd017';
   }
@@ -159,7 +167,8 @@ info.update = function (props) {
         'Town: ' + checkNull(props.properties.town) + '<br />' + 'Count: ' + checkNull(props.properties.Black) + '<br />' + '% of Black Arrested: ' + checkNull(props.properties.BP) +'<br />' +
         'PerCapita: ' + checkNull(props.properties[tabnum]) + '<br />' + 'Black Population: ' + checkNull(props.properties.BlackAlone) + '<br />' + 'Average bond: ' + checkNull(props.properties.TotalBbond) + '<br />' + 'White Pop.: ' + checkNull(props.properties.WhiteAlone) + '<br />' + 'Asian Pop.: ' + checkNull(props.properties.AsianAlone) +'<br />' + 'Hispanic Pop.: ' + checkNull(props.properties.Hispanicalone) 
         : 'Hover over a town');
-  } else {
+  } else if(tabnum.localeCompare("1") == 0) {
+    var pCapita = props.properties[tabnum]
     this._div.innerHTML = '<h4>Number of People Arrest</h4>' +  (props ?
         'Town: ' + checkNull(props.properties.town) + '<br />' + 'Count: ' + checkNull(props.properties.Count)
          + '<br />' + 'Asian Arrested: ' + checkNull(props.properties.Asian) + '<br />' + 'Black Arrested: ' + checkNull(props.properties.Black) + '<br />'+ 'Hispanic Arrested: ' + checkNull(props.properties.Hispanic) + '<br />'+ 'Population: ' + checkNull(props.properties.Population) + '<br />'+'Bond Average: ' + checkNull(props.properties.TotalAveB)
