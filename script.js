db7ce4f06b597f4b953b6bae6e77b636ec879b00
bvo773 <binhvo07@gmail.com>
2016-04-27 23:11:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // See Leaflet tutorial http://leafletjs.com/examples/map-panes.html
 // template by http://github.com/jackdougherty/leaflet-map/
 var tabnum = "0";
+
 geoJsonLayer.setStyle(style);
 legend.update(tabnum);
 resetAttribute(tabnum);
